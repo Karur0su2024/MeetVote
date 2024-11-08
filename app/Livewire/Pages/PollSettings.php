f715b4ff01a7821f8971258aec89ab33639f6747
Karur0su <50730505+Karur0su2024@users.noreply.github.com>
2024-11-08 12:24:12 +0100

    Revert "Added settings"
    This reverts commit fa824717cc846bad9b8dbd63b158e6bdf64de35f.

--- app/Livewire/Pages/PollSettings.php
+++ app/Livewire/Pages/PollSettings.php
@@ -3,24 +3,11 @@
 namespace App\Livewire\Pages;
 
 use Livewire\Component;
-use App\Livewire\Forms\PollSettingForm;
 
-class PollSettings extends Component {
-
-    public PollSettingForm $form;
-
+class PollSettings extends Component
+{
     public function render()
     {
         return view('livewire.pages.poll-settings')->layout('layouts.app', ['padding' => '5']);
     }
-
-    public function savePollSettings()
-    {
-        $info = $this->validate();
-
-        session(['poll' => $info]);
-        return $this->redirectRoute('polls.new.questions');
-    }
-
-
 }
--- resources/views/livewire/pages/poll-settings.blade.php
+++ resources/views/livewire/pages/poll-settings.blade.php
@@ -1,28 +1,3 @@
-<form class="container" wire:submit="savePollSettings">
-    <x-card col="8" class="p-3">
-        <h4 class="mb-3">{{ __('Basic information') }}</h4>
-
-        {{-- Název ankety --}}
-        <x-form.text-input type="text" name="form.title" id="title" label="Title" model="form.title" />
-
-
-        {{-- Popisek ankety --}}
-        <div class="mb-3">
-            <label class="form-label mb-1" for="description"> {{ __('Description') }}</label>
-            <textarea class="form-control" name="description" id="description" rows="5" wire:model='form.description'></textarea>
-            @error('form.description')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
-        </div>
-
-        <x-form.text-input type="text" name="form.username" id="username" label="{{ __('Your name') }}"
-            model="form.username" />
-        <x-form.text-input type="email" name="form.email" id="email" label="{{ __('E-mail') }}"
-            model="form.email" />
-
-    </x-card>
-
-    <div class="text-center mx-auto">
-        <x-button type="submit" style="primary" size="md" class="w-50 mt-5">{{ __('Submit') }}</x-button>
-    </div>
-</form>
+<div>
+    {{-- In work, do what you enjoy. --}}
+</div>
