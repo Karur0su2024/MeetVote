9289bdd3d25b44d29e7bc269a744d4897d51400b
Karur0su <50730505+Karur0su2024@users.noreply.github.com>
2025-03-22 19:10:59 +0100

    Opraven translated text v sekci nastavení ankety

--- resources/views/components/pages/poll-editor/settings-section.blade.php
+++ resources/views/components/pages/poll-editor/settings-section.blade.php
@@ -4,56 +4,56 @@
     {{-- Komentáře --}}
     <x-ui.form.checkbox id="comments" x-model="form.settings.comments">
         <x-slot:tooltip>
-            {{ __('form.tooltip.comments') }}
+            {{ __('pages/poll-editor.settings.comments.tooltip') }}
         </x-slot:tooltip>
-        {{ __('form.label.comments') }}
+        {{ __('pages/poll-editor.settings.comments.label') }}
     </x-ui.form.checkbox>
 
     {{-- Tajné hlasování --}}
     <x-ui.form.checkbox id="anonymous" x-model="form.settings.anonymous">
         <x-slot:tooltip>
-            {{ __('form.tooltip.anonymous') }}
+            {{ __('pages/poll-editor.settings.anonymous.tooltip') }}
         </x-slot:tooltip>
-        {{ __('form.label.anonymous') }}
+        {{ __('pages/poll-editor.settings.anonymous.label') }}
     </x-ui.form.checkbox>
 
     {{-- Skrytí výsledků --}}
     <x-ui.form.checkbox id="hide_results" x-model="form.settings.hide_results">
         <x-slot:tooltip>
-            {{ __('form.tooltip.hide_results') }}
+            {{ __('pages/poll-editor.settings.hide_results.tooltip') }}
         </x-slot:tooltip>
-        {{ __('form.label.hide_results') }}
+        {{ __('pages/poll-editor.settings.hide_results.label') }}
     </x-ui.form.checkbox>
 
     {{-- Účástníci mohou změnit své odpovědi --}}
     <x-ui.form.checkbox id="edit_votes" x-model="form.settings.edit_votes">
         <x-slot:tooltip>
-            {{ __('form.tooltip.edit_votes') }}
+            {{ __('pages/poll-editor.settings.edit_votes.tooltip') }}
         </x-slot:tooltip>
-        {{ __('form.label.edit_votes') }}
+        {{ __('pages/poll-editor.settings.edit_votes.label') }}
     </x-ui.form.checkbox>
 
     {{-- Pouze pro pozvané --}}
     <x-ui.form.checkbox id="invite_only" x-model="form.settings.invite_only">
         <x-slot:tooltip>
-            {{ __('form.tooltip.invite_only') }}
+            {{ __('pages/poll-editor.settings.invite_only.tooltip') }}
         </x-slot:tooltip>
-        {{ __('form.label.invite_only') }}
+        {{ __('pages/poll-editor.settings.invite_only.label') }}
     </x-ui.form.checkbox>
 
     {{-- Přidání časových možností --}}
     <x-ui.form.checkbox id="add_time_options" x-model="form.settings.add_time_options">
         <x-slot:tooltip>
-            {{ __('form.tooltip.add_time_options') }}
+            {{ __('pages/poll-editor.settings.add_time_options.label') }}
         </x-slot:tooltip>
-        {{ __('form.label.add_time_options') }}
+        {{ __('pages/poll-editor.settings.add_time_options.label') }}
     </x-ui.form.checkbox>
 
     {{-- Heslo --}}
     <x-ui.form.input id="password" x-model="form.settings.password" type="password" error="form.settings.password" error="form.settings.password">
         <x-slot:tooltip>
-            {{ __('form.tooltip.password') }}
+            {{ __('pages/poll-editor.settings.password.tooltip') }}
         </x-slot:tooltip>
-        {{ __('form.label.password') }}
+        {{ __('pages/poll-editor.settings.password.label') }}
     </x-ui.form.input>
 </x-card>
