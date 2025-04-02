d3fd90a6f4b0f10d1a0cef1fd77203e653718afd
Karur0su <50730505+Karur0su2024@users.noreply.github.com>
2025-04-02 17:10:27 +0200

    Zprovoznění Sailu

--- vite.config.js
+++ vite.config.js
@@ -10,11 +10,11 @@ export default defineConfig({
     ],
     server: {
         host: '0.0.0.0',
-        port: 5180,
+        port: 5173,
         strictPort: true,
-        cors: true,
         hmr: {
-            host: '192.168.1.131',
+            host: 'localhost',
+            port: 5173,
             protocol: 'ws',
         },
     },
