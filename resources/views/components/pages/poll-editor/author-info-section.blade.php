4a1e9c9c89cd0684f9b4ed28195af8196242b504
Karur0su <50730505+Karur0su2024@users.noreply.github.com>
2025-04-08 18:28:37 +0200

    refactor: odstranění anonymního zveřejnění
    Nepotřebná funkce, není důvod ji zatím do aplikace přidávat

--- resources/views/components/pages/poll-editor/author-info-section.blade.php
+++ resources/views/components/pages/poll-editor/author-info-section.blade.php
@@ -1,44 +1,29 @@
-<div x-data="{ anonymous: @entangle('form.user.posted_anonymously') }">
+<div>
 
     <h4 class="text-muted mb-3">
         User information
     </h4>
 
-    @auth
-        {{-- Nastavení anonymity autora --}}
-        <x-ui.form.checkbox
-            id="show.user-info"
-            x-model="anonymous">
-            <x-slot:tooltip>
-                {{ __('pages/poll-editor.basic_info.post_anonymously.tooltip') }}
-            </x-slot:tooltip>
-            {{ __('pages/poll-editor.basic_info.post_anonymously.label') }}
-        </x-ui.form.checkbox>
-    @endauth
 
+    {{-- Jméno autora --}}
+    <x-ui.form.input
+        id="user_name"
+        x-model="form.user.name"
+        type="text"
+        required
+        placeholder="{{ __('pages/poll-editor.basic_info.user_name.placeholder') }}"
+        x-error="form.user.name">
+        {{ __('pages/poll-editor.basic_info.user_name.label') }}
+    </x-ui.form.input>
 
-    <div x-show="!anonymous" x-collapse>
-
-        {{-- Jméno autora --}}
-        <x-ui.form.input
-            id="user_name"
-            x-model="form.user.name"
-            type="text"
-            required
-            placeholder="{{ __('pages/poll-editor.basic_info.user_name.placeholder') }}"
-            x-error="form.user.name">
-            {{ __('pages/poll-editor.basic_info.user_name.label') }}
-        </x-ui.form.input>
-
-        {{-- E-mail autora --}}
-        <x-ui.form.input
-            id="user_email"
-            x-model="form.user.email"
-            type="email"
-            required
-            placeholder="{{ __('pages/poll-editor.basic_info.user_email.placeholder') }}"
-            x-error="form.user.email">
-            {{ __('pages/poll-editor.basic_info.user_email.label') }}
-        </x-ui.form.input>
-    </div>
+    {{-- E-mail autora --}}
+    <x-ui.form.input
+        id="user_email"
+        x-model="form.user.email"
+        type="email"
+        required
+        placeholder="{{ __('pages/poll-editor.basic_info.user_email.placeholder') }}"
+        x-error="form.user.email">
+        {{ __('pages/poll-editor.basic_info.user_email.label') }}
+    </x-ui.form.input>
 </div>
