09ca0c27c44fd401dc41cb09d8ffcb367ffe6a87
Karur0su <50730505+Karur0su2024@users.noreply.github.com>
2025-04-09 17:16:53 +0200

    chore: přidáno jednoduché logování pro Google kalendář

--- app/Listeners/SyncWithGoogleCalendar.php
+++ app/Listeners/SyncWithGoogleCalendar.php
@@ -6,6 +6,7 @@ use App\Events\PollEventCreated;
 use App\Services\Google\GoogleService;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
 
 class SyncWithGoogleCalendar
 {
@@ -25,8 +26,16 @@ class SyncWithGoogleCalendar
     public function handle(PollEventCreated $pollEventCreated): void
     {
         $poll = $pollEventCreated->poll;
-        $users = $poll->votes()->with('user')->get()->pluck('user')->unique()->filter();
-        $event = $poll->event;
-        $this->googleService->syncWithGoogleCalendar($users, $event);
+        Log::info('Syncing with Google Calendar...', ['poll_id' => $poll->id]);
+
+        try {
+
+            $users = $poll->votes()->with('user')->get()->pluck('user')->unique()->filter();
+            $event = $poll->event;
+            $this->googleService->syncWithGoogleCalendar($users, $event);
+        } catch (\Exception $e) {
+            Log::error('Error while syncing calendar: ' . $e->getMessage());
+        }
+
     }
 }
