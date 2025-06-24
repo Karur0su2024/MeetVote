725549f0be344f43a50b68ad4d8f546e70baf0f8
Karur0su <50730505+Karur0su2024@users.noreply.github.com>
2025-06-24 06:09:30 +0200

    feat: automatické zjištění jazyka z prohlížeče

--- app/Http/Middleware/SetLanguage.php
+++ app/Http/Middleware/SetLanguage.php
@@ -12,7 +12,18 @@ class SetLanguage
     // Nastavení jazyka uloženého v session
     public function handle(Request $request, Closure $next): Response
     {
-        App::setLocale(session()->get('language', 'en'));
+
+        // Rozšíření o kontrolu jazyka v session
+        // Pokud není jazyk v session, nastavíme ho na základě hlavičky Accept-Language
+        if(!session()->has('language')) {
+            $lang = substr($request->header('Accept-Language'), 0, 2);
+            if(in_array($lang, ['en', 'cs'])) {
+                session()->put('language', $lang);
+            } else{
+                session()->put('language', 'en');
+            }
+        }
+        App::setLocale(session()->get('language'));
 
         return $next($request);
     }
